2cbf74d0ca2dcb43f7a4707ddc9085abab64bbd5
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-07-07 09:40:35 +0000

    Merge Multi-Level Double Linked List

--- Linked Lists Questions/Merge Multi-Level Double Linked List.py
+++ Linked Lists Questions/Merge Multi-Level Double Linked List.py
@@ -1,2 +1,24 @@
 def flatten(head):
+	if (head is None):
+		return head	
 	
+	currNode = head
+
+	while(currNode is not None):
+
+		if(currNode.child is None):
+			currNode = currNode.next
+
+		else:
+			tail = currNode.child
+			while(tail.next is not None):
+				tail = tail.next
+
+			tail.next = currNode.next
+			if(tail.next is not None):
+				tail.next.prev = tail
+			currNode.next = currNode.child
+			currNode.next.prev = currNode
+			currNode.child = None
+
+	return head
